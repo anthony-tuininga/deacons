8db3079025cbc2e17cc17a9b46e5b77588266263
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2007-11-15 04:02:39 +0000

    Added simple window for editing the list of elders.

---
+++ w_Elders.py
@@ -0,0 +1,26 @@
+import ceDatabase
+import ceGUI
+
+class EditWindow(ceGUI.GridEditWindow):
+    title = "Elders"
+
+    def OnCreate(self):
+        self.AddSubWindow(DistrictSubWindow)
+
+
+class Grid(ceGUI.Grid):
+    
+    def OnCreate(self):
+        self.AddColumn(ceGUI.GridColumnStr, "Name", "name")
+
+
+class DataSet(ceDatabase.DataSet):
+    tableName = "Elders"
+    attrNames = "elderId name"
+    pkAttrNames = "elderId"
+
+
+class DistrictSubWindow(ceGUI.SubWindow):
+    childWindowName = "w_ElderDistricts.EditWindow"
+    label = "District"
+
