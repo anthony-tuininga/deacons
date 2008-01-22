960f965581acc365ad72fb0d4711a61386d25b5a
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2008-01-22 03:33:28 +0000

    Use a notebook and panels rather than windows to manage editing data.

--- w_TopLevelFrame.py
+++ w_TopLevelFrame.py
@@ -1,8 +1,27 @@
 import ceGUI
 import wx
 
+from wx.lib.flatnotebook import FlatNotebook
+
 class Frame(ceGUI.TopLevelFrame):
     title = "Deacons"
+    createToolbar = False
+
+    def __AddPage(self, name, text):
+        currentPage = self.notebook.GetCurrentPage()
+        newPage = ceGUI.OpenWindow(name, self.notebook, instanceName = text)
+        if newPage is currentPage:
+            return newPage
+        if currentPage is not None:
+            currentPage.SaveSettings()
+        for pageIndex in range(self.notebook.GetPageCount()):
+            page = self.notebook.GetPage(pageIndex)
+            if newPage is page:
+                self.notebook.SetSelection(pageIndex)
+                return newPage
+        self.notebook.AddPage(newPage, text)
+        newPage.RestoreSettings()
+        return newPage
 
     def __CreateEditMenu(self):
         menu = self.AddMenu("&Edit")
@@ -12,7 +31,7 @@ class Frame(ceGUI.TopLevelFrame):
     def __CreateFileMenu(self):
         menu = self.AddMenu("&File")
         self.AddMenuItem(menu, "&Elders", "Edit elders",
-                windowName = "w_Elders.EditWindow")
+                method = self.OnEditElders, passEvent = False)
         self.AddMenuItem(menu, "&Causes", "Edit causes",
                 windowName = "w_Causes.EditWindow")
         self.AddMenuItem(menu, "De&posits", "Edit deposits",
@@ -28,14 +47,28 @@ class Frame(ceGUI.TopLevelFrame):
         menu = self.AddMenu("&Help")
         self.AddStockMenuItem(menu, wx.ID_ABOUT, self.OnAbout)
 
+    def OnCreate(self):
+        self.notebook = FlatNotebook(self,
+                style = wx.lib.flatnotebook.FNB_NO_NAV_BUTTONS | \
+                        wx.lib.flatnotebook.FNB_NO_X_BUTTON | \
+                        wx.lib.flatnotebook.FNB_X_ON_TAB)
+        self.BindEvent(self.notebook,
+                wx.lib.flatnotebook.EVT_FLATNOTEBOOK_PAGE_CLOSING,
+                self.OnPageClosing, skipEvent = False)
+
     def OnCreateMenus(self):
         self.__CreateFileMenu()
         self.__CreateEditMenu()
         self.__CreateHelpMenu()
 
-    def OnCreateToolbar(self):
-        self.AddToolbarItem("Exit", wx.ART_QUIT,
-                shortHelp = "Exit the application",
-                longHelp = "Exit the application.",
-                method = self.OnExit)
+    def OnEditElders(self):
+        self.__AddPage("w_Elders.Panel", "Elders")
+
+    def OnPageClosing(self, event):
+        page = self.notebook.GetCurrentPage()
+        if not page.ContinueQuery():
+            event.Veto()
+        else:
+            page.SaveSettings()
+            event.Skip()
 
