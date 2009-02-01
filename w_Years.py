5f1b635a26365f74176e631a610c5c3709c19fa9
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2009-02-01 05:28:21 +0000

    Restored ability to create tax receipts for a given year.

--- w_Years.py
+++ w_Years.py
@@ -1,5 +1,11 @@
+"""
+Panel displaying information about years.
+"""
+
 import ceDatabase
 import ceGUI
+import cx_Logging
+import datetime
 import wx
 
 import Common
@@ -9,6 +15,126 @@ class Panel(ceGUI.DataListPanel):
 
 
 class List(ceGUI.DataList):
+
+    def __AskQuestion(self, message, title):
+        dialog = wx.MessageDialog(self, message, title,
+                wx.YES_NO | wx.ICON_QUESTION)
+        response = dialog.ShowModal()
+        dialog.Destroy()
+        return (response == wx.ID_YES)
+
+    def __CheckReceipts(self, row):
+        if not row.promptForReceiptGeneration:
+            return
+        message = "Calculate tax receipts for year %d?" % row.year
+        if self.__AskQuestion(message, "Calculate Tax Receipts?"):
+            self.__GenerateReceipts(row)
+            return
+        message = "Do not ask about calculating tax receipts for %s again?" % \
+                row.year
+        if self.__AskQuestion(message, "Ask About Receipt Calculation?"):
+            cursor = self.config.connection.cursor()
+            cursor.execute("""
+                    update Years set
+                        PromptForReceiptGeneration = 'f'
+                    where Year = ?""", row.year)
+            self.config.connection.commit()
+            row.promptForReceiptGeneration = False
+
+    def __GenerateReceipts(self, row):
+        cx_Logging.Debug("generating receipts for year %s", row.year)
+        origReceipts = self.__GetOriginalReceipts(row)
+        cx_Logging.Debug("%s original receipts", len(origReceipts))
+        maxReceiptNum = self.__GetNextReceiptNum()
+        cx_Logging.Debug("max receipt number is %s", maxReceiptNum)
+        donatedAmounts = self.__GetDonatedAmounts(row)
+        cx_Logging.Debug("%s donations to receipt", len(donatedAmounts))
+        today = datetime.date.today().strftime("%Y-%m-%d")
+        receiptsCanceled = receiptsUpdated = receiptsCreated = 0
+        cursor = self.config.connection.cursor()
+        for name, donatorId, amount in donatedAmounts:
+            if donatorId in origReceipts:
+                origReceiptNumber, origAmount = origReceipts[donatorId]
+            else:
+                origReceiptNumber = origAmount = None
+            if amount == origAmount:
+                continue
+            if row.receiptsIssued and origAmount is not None:
+                receiptsCanceled += 1
+                cursor.execute("""
+                        update TaxReceipts set
+                            Canceled = 't'
+                        where ReceiptNumber = ?""", origReceiptNumber)
+            if not row.receiptsIssued and origAmount is not None:
+                receiptsUpdated += 1
+                cursor.execute("""
+                        update TaxReceipts set
+                            Amount = ?
+                        where ReceiptNumber = ?""",
+                        Common.FormattedAmount(amount), origReceiptNumber)
+            else:
+                receiptsCreated += 1
+                maxReceiptNum += 1
+                formattedAmount = Common.FormattedAmount(amount)
+                cursor.execute("""
+                        insert into TaxReceipts
+                        (ReceiptNumber, Year, DonatorId, Amount, Canceled,
+                         DateIssued, IsDuplicate)
+                        values (?, ?, ?, ?, 'f', ?, 'f')""",
+                        maxReceiptNum, row.year, donatorId, formattedAmount,
+                        today)
+        self.config.connection.commit()
+        message = "%d canceled. %d updated. %d created" % \
+                (receiptsCanceled, receiptsUpdated, receiptsCreated)
+        wx.MessageBox(message, "Receipt Generation Results",
+                wx.OK | wx.ICON_INFORMATION, self)
+
+    def __GetDonatedAmounts(self, row):
+        cursor = self.config.connection.cursor()
+        cursor.execute("""
+                select
+                  d.DonatorId,
+                  sum(d.Amount)
+                from
+                  CausesForYear cy,
+                  Donations d
+                where d.ClaimYear = ?
+                  and d.DonatorId is not null
+                  and cy.CauseId = d.CauseId
+                  and cy.Year = d.ClaimYear
+                  and cy.Deductible = 't'
+                group by d.DonatorId""",
+                row.year)
+        amounts = []
+        for donatorId, amount in cursor:
+            donator = self.config.cache.DonatorForId(donatorId)
+            amounts.append((donator.name, donatorId, amount))
+        amounts.sort()
+        return amounts
+
+    def __GetNextReceiptNum(self):
+        cursor = self.config.connection.cursor()
+        cursor.execute("""
+                select max(ReceiptNumber)
+                from TaxReceipts""")
+        maxReceiptNum, = cursor.fetchone()
+        return maxReceiptNum
+
+    def __GetOriginalReceipts(self, row):
+        origReceipts = {}
+        cursor = self.config.connection.cursor()
+        cursor.execute("""
+                select
+                  ReceiptNumber,
+                  DonatorId,
+                  Amount
+                from TaxReceipts
+                where Year = ?
+                  and Canceled = 'f'""",
+                row.year)
+        for receiptNumber, donatorId, amount in cursor:
+            origReceipts[donatorId] = (receiptNumber, amount)
+        return origReceipts
 
     def _CreateContextMenu(self):
         super(List, self)._CreateContextMenu()
@@ -45,12 +171,14 @@ class List(ceGUI.DataList):
 
     def OnTaxReceipts(self):
         app = wx.GetApp()
-        selectedItems = self.GetSelectedItems()
-        app.topWindow._AddTaxReceiptsPage(selectedItems[0].year)
+        item, = self.GetSelectedItems()
+        self.__CheckReceipts(item)
+        app.topWindow._AddTaxReceiptsPage(item.year)
 
 
 class DataSet(ceDatabase.DataSet):
     tableName = "Years"
-    attrNames = "year budgetAmount"
+    attrNames = "year budgetAmount promptForReceiptGeneration receiptsIssued"
+    charBooleanAttrNames = "promptForReceiptGeneration receiptsIssued"
     pkAttrNames = "year"
 
