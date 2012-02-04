1d215c7a3ac915b1dd750fdb6cee74ccee95d59f
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2012-02-04 16:54:00 +0000

    Add support for using remote database server; tidy up configuration to take into account changes made to the library.

--- Config.py
+++ Config.py
@@ -2,17 +2,20 @@
 Configuration for Deacons application.
 """
 
+import ceGUI
 import ceODBC
 import os
 
 import Cache
 
-class Config(object):
-    baseDsn = 'Driver=PostgreSQL;Servername=localhost;Database=deacons;' \
-            'readonly=0'
+class Config(ceGUI.Config):
+    baseDsn = 'Driver=PostgreSQL;Servername=%s;Database=deacons;readonly=0'
 
     def __init__(self, app):
-        dsn = "%s;uid=%s" % (self.baseDsn, os.environ["LOGNAME"])
+        super(Config, self).__init__(app)
+        server = os.environ.get("DEACONS_DB_SERVER", "localhost")
+        baseDsn = self.baseDsn % server
+        dsn = "%s;uid=%s" % (baseDsn, os.environ["LOGNAME"])
         self.connection = Connection(dsn)
         self.cache = app.cache = Cache.Cache(self.connection)
         app.copyAttributes.append("cache")
