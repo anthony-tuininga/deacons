93dfb5e28d9355d566bb1852897547d5147b1c95
Anthony Tuininga <anthony.tuininga@oracle.com>
2017-02-04 18:44:09 -0700

    Added a filter by name to the donators panel.

--- Panels/Donators.py
+++ Panels/Donators.py
@@ -8,6 +8,11 @@ import Models
 class Panel(ceGUI.DataListPanel):
     editDialogName = "EditDialogs.Donators.Dialog"
 
+    def OnCreateFilterArgs(self):
+        ceGUI.FilterArgStr(self, "lastName", "Last Name:", size = (150, -1))
+        ceGUI.FilterArgStr(self, "givenNames", "Given Names:",
+                size = (150, -1))
+
 
 class List(ceGUI.DataList):
 
@@ -18,4 +23,13 @@ class List(ceGUI.DataList):
 
 class DataSet(ceGUI.DataSet):
     rowClass = Models.Donators
+
+    def _GetRows(self, lastName, givenNames):
+        rows = self.config.GetCachedRows(self.rowClass)
+        if lastName is not None:
+            rows = [r for r in rows if lastName in r.lastName.upper()]
+        if givenNames is not None:
+            rows = [r for r in rows if r.givenNames is not None \
+                    and givenNames in r.givenNames.upper()]
+        return rows
 
