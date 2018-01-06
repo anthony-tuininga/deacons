cac8a59adcf3947cd138bbb6ed99e3c9259e684a
Anthony Tuininga <anthony.tuininga@oracle.com>
2018-01-06 14:51:40 -0700

    Bump copyright into 2018.

--- Deacons.py
+++ Deacons.py
@@ -8,7 +8,7 @@ class Deacons(ceGUI.Application):
     description = "Deacons"
     copyrightOwner = "Anthony Tuininga"
     copyrightStartYear = 1998
-    copyrightEndYear = 2017
+    copyrightEndYear = 2018
     configClassName = "Config.Config"
 
 
