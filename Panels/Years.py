5965301b607be3809608918284773cc52c299cb5
Anthony Tuininga <anthony.tuininga@oracle.com>
2018-01-13 12:10:59 -0700

    Duplicate causes and donators for the year when a new year is created.

--- Panels/Years.py
+++ Panels/Years.py
@@ -3,6 +3,8 @@ Panel displaying information about years.
 """
 
 import ceGUI
+import cx_Logging
+import decimal
 
 import Common
 import Models
@@ -24,4 +26,44 @@ class Grid(Common.BaseGrid):
 
 class DataSet(ceGUI.DataSet):
     rowClass = Models.Years
+
+    def _OnInsertRow(self, row, choice):
+        row.budgetAmount = decimal.Decimal(0)
+        row.receiptsIssued = False
+
+    def InsertRowInDatabase(self, transaction, row):
+        cursor = self.dataSource.connection.cursor()
+        cursor.execute("""
+                select max(Year)
+                from Years
+                where Year < ?""",
+                row.year)
+        fetchedRow = cursor.fetchone()
+        super(DataSet, self).InsertRowInDatabase(transaction, row)
+        if fetchedRow is not None:
+            copyFromYear, = fetchedRow
+            cx_Logging.Info("Copying causes and donators from year %s",
+                    copyFromYear)
+            for cause in Models.Causes.GetRows(self.dataSource,
+                    year = copyFromYear):
+                setValues = dict(year = row.year,
+                        description = cause.description,
+                        deductible = cause.deductible,
+                        reported = cause.reported,
+                        notes = cause.notes,
+                        donationAccountCode = cause.donationAccountCode,
+                        looseCashAccountCode = cause.looseCashAccountCode)
+                transaction.AddItem(tableName = Models.Causes.tableName,
+                        pkAttrName = Models.Causes.pkAttrNames[0],
+                        pkSequenceName = "CauseId_s", setValues = setValues)
+            for donator in Models.Donators.GetRows(self.dataSource,
+                    year = copyFromYear):
+                setValues = dict(year = row.year,
+                        surname = donator.surname,
+                        givenNames = donator.givenNames,
+                        assignedNumber = donator.assignedNumber,
+                        address = donator.address)
+                transaction.AddItem(tableName = Models.Donators.tableName,
+                        pkAttrName = Models.Donators.pkAttrNames[0],
+                        pkSequenceName = "DonatorId_s", setValues = setValues)
 
